d74f539ece23fec5ecf2edf5bed98e744d1ab612
Piotr Dabrowski <piotr.dabrowski@pb.com>
2023-08-25 12:07:28 +0200

    added new logic for navabar in JS

--- mainJS.js
+++ mainJS.js
@@ -1,6 +1,33 @@
+const navbar = document.getElementById('main-navbar');
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const initialScrollPos = window.pageYOffset;
+let prevScrollPos = window.pageYOffset;
+let isNavbarVisible = true;
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 });
+window.addEventListener('scroll', () => { 
+    const currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos < currentScrollPos) { 
+        navbar.style.top = `-${navbar.offsetHeight}px`;
+    } else if (currentScrollPos == initialScrollPos) {
+        navbar.style.top = "0";
+    }
+
+    prevScrollPos = currentScrollPos;
+});
+/*
+navbar.addEventListener("mouseover", () => { 
+    console.log("mouseover");
+    navbar.style.top = "0";
+});*/
+/*
+navbar.addEventListener("mouseleave", () => { 
+    if (window.pageYOffset > navbar.offsetHeight) {
+        navbar.style.top = `-${navbar.offsetHeight}px`;
+    }
+});
+*/
